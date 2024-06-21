f8ef6a2dec7cbc222f1471834e204739222f832d
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-06-21 18:15:13 +0530

    map added in pal_loc_card

--- lib/Caregiver-App/Caregiver-Home/widgets/pal_loc_card.dart
+++ lib/Caregiver-App/Caregiver-Home/widgets/pal_loc_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class PalLocCard extends StatefulWidget {
   const PalLocCard({super.key});
@@ -15,12 +17,51 @@ class _PalLocCardState extends State<PalLocCard> {
     return Stack(
       children: [
         Container(
-          height: screenHeight * 0.28,
-          width: screenWidth * 0.92,
-          decoration: BoxDecoration(
-              color: Color(0xff525252),
-              borderRadius: BorderRadius.circular(30)),
-        ),
+            height: screenHeight * 0.28,
+            width: screenWidth * 0.92,
+            decoration: BoxDecoration(
+                color: Color(0xff525252),
+                borderRadius: BorderRadius.circular(30)),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Container(
+                  width: screenWidth * 0.92,
+                  height: screenHeight * 0.202,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30),
+                          bottomRight: Radius.circular(30))),
+                  child: FlutterMap(
+                    options: MapOptions(
+                      initialZoom: 10,
+                      initialCenter: LatLng(19.4056, 72.8557)
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                      CircleLayer(
+  circles: [
+    CircleMarker(
+      point: LatLng(19.4056, 72.8557), // center of 't Gooi
+      radius: 500,
+      useRadiusInMeter: true,
+      color: Colors.red.withOpacity(0.3),
+      borderColor: Colors.red.withOpacity(0.7),
+      borderStrokeWidth: 2,
+    )
+  ],
+)
+                    ],
+                  ),
+                )
+              ],
+            )),
         Container(
           height: screenHeight * 0.078,
           width: screenWidth * 0.92,
